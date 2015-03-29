fcb275ba574a8809e17555bb70f0817b8210eb73
Lifepillar <github@lifepillar.com>
2015-03-29 14:16:25 +0200

    New formula: Jace 2013-04-07”.

---
+++ jace.rb
@@ -0,0 +1,16 @@
+class Jace < Formula
+  homepage "https://sites.google.com/site/brendanrobert/projects/jace"
+  url "https://downloads.sourceforge.net/project/java-ace/Builds/jace.jar"
+  version "2013-04-07"
+  sha256 "3f6d0227f46f7a8cacce5e8acdc57aca8cc09803fe680ff6f4aced62d3971754"
+
+  depends_on :java => "1.6+"
+
+  def install
+    libexec.install "jace.jar"
+    bin.write_jar_script libexec/"jace.jar", "jace"
+  end
+
+  test do
+  end
+end
