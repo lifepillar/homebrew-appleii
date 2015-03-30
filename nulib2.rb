274ee6046f245dcbdea0ba35f21bf9f187cfea73
Lifepillar <github@lifepillar.com>
2015-03-30 17:16:41 +0200

    New formula: Nulib2/NufxLib v3.0.0.

---
+++ nulib2.rb
@@ -0,0 +1,21 @@
+class Nulib2 < Formula
+  homepage "http://nulib.com"
+  url "https://github.com/fadden/nulib2/archive/v3.0.0.tar.gz"
+  sha256 "0a0b33cf459d3b2a053f5ef6bbe9b0407cac3ab5e3f617fda06da3b6df949f85"
+
+  def install
+    cd "nufxlib" do
+      system "./configure", "--prefix=#{prefix}"
+      system "make", "install"
+    end
+
+    cd "nulib2" do
+      system "./configure", "--prefix=#{prefix}"
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "false"
+  end
+end
