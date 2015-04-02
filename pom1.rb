80769310587b7ea499c6d484e9c8861425ecc904
Lifepillar <github@lifepillar.com>
2015-04-02 10:27:04 +0300

    New formula: Pom1 v1.0.0.

---
+++ pom1.rb
@@ -0,0 +1,18 @@
+class Pom1 < Formula
+  homepage "http://pom1.sourceforge.net"
+  url "https://downloads.sourceforge.net/project/pom1/pom1/1.0.0/pom1-1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "44f7c5278773397cc98d14b5ff3422770f05caa4e8af5f65487c24aaa82ebc85"
+
+  depends_on "sdl"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install" 
+  end
+
+  test do
+    system "false"
+  end
+end
