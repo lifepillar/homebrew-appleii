45f95179c5fb84ba8ea02c134ee7691192f35ff5
Lifepillar <github@lifepillar.com>
2015-04-02 17:07:15 +0300

    Use stable version of epple2.
    We now use the stable version of Epple2, and leave
    the Github source as an option.
    
    We have added Epple-II-Rom (epple2sys) as a resource, so that
    epple2 won't warn about missing files on launch.
    
    Note that we have been able to build and run the HEAD. The stable
    version does not build on OS X Lion (we haven't tried other platforms
    yet).

--- epple2.rb
+++ epple2.rb
@@ -1,25 +1,42 @@
 class Epple2 < Formula
   homepage "http://mosher.mine.nu/epple2"
-  url "https://github.com/cmosher01/Epple-II.git"
-  sha1 "ec13fbdaf14953a234f49b80dfda6a9502e18479"
+  url "http://mosher.mine.nu/epple2/download/epple2-latest.tar.gz"
+  version "1.1"
+  sha256 "978c7c9f72ea1bdd6352aeae5058855924fdfa9418fcd2c608959d4884cbff30"
+  head "https://github.com/cmosher01/Epple-II.git"
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
+  depends_on :x11
+  depends_on "sdl2"
+  # For epple2sys:
+  depends_on "m4" => :build
+  depends_on "xa" => :build
 
   needs :cxx11
+
+  resource "epple2sys" do
+    url "http://mosher.mine.nu/epple2/download/epple2sys-latest.tar.gz"
+    version "1.1"
+    sha256 "653c89bde1c4093d625d8b9fe6b8b008b43ccbe6160d92a02965153508450638"
+  end
 
   def install
     ENV.cxx11
 
-    system "autoreconf --install"
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+    resource("epple2sys").stage do
+      system "./configure", "--prefix=#{prefix}"
+      system "make", "install"
+    end
+
+    opts = []
+    opts << "--disable-dependency-tracking"
+    opts << "--disable-silent-rules" if build.head?
+    opts << "--prefix=#{prefix}"
+
+    system "./configure", *opts
     system "make", "install"
   end
 
   test do
-    system "true"
+    system "false"
   end
 end
