61cc64762298f51c7822eee50e08f250a566526d
Lifepillar <github@lifepillar.com>
2015-04-19 21:56:59 +0200

    epple2: add revision option.
    Add a revision tag to the Git download, which is
    considered stable. The latest commit from master
    can be installed with —HEAD.

--- epple2.rb
+++ epple2.rb
@@ -1,6 +1,8 @@
 class Epple2 < Formula
   homepage "http://mosher.mine.nu/epple2"
-  url "https://github.com/cmosher01/Epple-II.git"
+  url "https://github.com/cmosher01/Epple-II.git", :revision => "130d2b4ac4333062f30d0e6ae5b668228e3cc36f"
+  version "130d2b4a"
+  head "https://github.com/cmosher01/Epple-II.git"
 
   depends_on "automake" => :build
   depends_on "autoconf" => :build
