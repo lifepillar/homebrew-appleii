74b34e34d9caa6eee4aafdaeee0e73fa31dafc43
Lifepillar <github@lifepillar.com>
2015-09-09 22:20:59 +0200

    New formula: Apple II Disk Tools.

---
+++ apple-ii-disk-tools.rb
@@ -0,0 +1,23 @@
+class AppleIiDiskTools < Formula
+  desc "Utilities for Apple ][ floppy disk images"
+  homepage "https://github.com/cmosher01/Apple-II-Disk-Tools"
+  head "https://github.com/cmosher01/Apple-II-Disk-Tools.git"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "gettext"
+
+  def install
+    system "./bootstrap --skip-po"
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "a2catalog -T"
+    system "a2nibblize -T"
+  end
+end
