cf05c8df2f3ee3471c9647ecacda93f21df56475
Lifepillar <github@lifepillar.com>
2015-09-10 22:26:33 +0200

    New cask: ADTPro 2.0.1.

---
+++ Casks/adtpro.rb
@@ -0,0 +1,14 @@
+cask :v1 => 'adtpro' do
+  version '2.0.1'
+  sha256 '26384b16d3d5b0bae1afde511be8c6f087d5ecd203d2a4197ff2874de4de860f'
+
+  adtpro = 'ADTPro-2.0.1'
+  url "http://downloads.sourceforge.net/sourceforge/adtpro/#{adtpro}.dmg"
+  name 'ADTPro'
+  name 'Apple Disk Transfer ProDOS'
+  homepage 'http://adtpro.sourceforge.net'
+  license :gratis
+
+  app "#{adtpro}/#{adtpro}.app"
+end
+
