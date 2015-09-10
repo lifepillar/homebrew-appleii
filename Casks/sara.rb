e4115d3d79229a4a043624e6777d702843daebd3
Lifepillar <github@lifepillar.com>
2015-09-10 23:10:44 +0200

    New cask: Sara Apple /// Emulator 0.5.1.

---
+++ Casks/sara.rb
@@ -0,0 +1,13 @@
+cask :v1 => 'sara' do
+  version '0.5.1'
+  sha256 '0822aa281849cf306d5bc02ea4e811563de2469687ac99bce0d0b9a929d4cfbe'
+
+  url 'http://www.blackcatsystems.com/download/SaraOSX.zip'
+  name 'Sara'
+  name 'Sara Apple /// Emulator'
+  homepage 'http://www.blackcatsystems.com/software/sara.html'
+  license :unknown
+
+  app 'SaraOSX/Sara.app'
+end
+
