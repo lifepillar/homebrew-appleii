09713c04a7db45383b7bb0e4c7d09dffd7d3a0d9
Lifepillar <github@lifepillar.com>
2015-09-10 22:37:14 +0200

    New cask: GSPort 0.31.

---
+++ Casks/gsport.rb
@@ -0,0 +1,16 @@
+cask :v1 => 'gsport' do
+  version '0.31'
+  sha256 '00d5df71ff0ee6932ece28b248e100fdb378d05d958374223c15962b2529b913'
+
+  url "http://downloads.sourceforge.net/sourceforge/gsport/gsport_0.31.dmg"
+  name 'GSPort'
+  homepage 'http://gsport.sourceforge.net'
+  license :gpl
+
+  app "GSPort/GSPort.app"
+  
+  caveats do
+    "A ROM file should be put in #{staged_path}/GSport."
+  end
+end
+
