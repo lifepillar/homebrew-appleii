b1c3aa4006e3e865152e31363a7eff595f208fbb
Lifepillar <github@lifepillar.com>
2015-12-27 08:23:40 +0200

    Allow installing NuLib2 from Github's master branch.

--- nulib2.rb
+++ nulib2.rb
@@ -3,6 +3,7 @@ class Nulib2 < Formula
   homepage "http://nulib.com"
   url "https://github.com/fadden/nulib2/archive/v3.0.0.tar.gz"
   sha256 "0a0b33cf459d3b2a053f5ef6bbe9b0407cac3ab5e3f617fda06da3b6df949f85"
+  head "https://github.com/fadden/nulib2.git"
 
   def install
     cd "nufxlib" do
