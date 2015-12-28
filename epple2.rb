ef35deefb2a77d5ec25103f262f5aad2d35c3abe
Lifepillar <github@lifepillar.com>
2015-12-28 14:51:02 +0200

    Update formula: Epple2.

--- epple2.rb
+++ epple2.rb
@@ -1,29 +1,31 @@
 class Epple2 < Formula
-  desc "An emulator of the original APPLE ][ and APPLE ][ PLUS computers"
+  desc "Emulator of the original APPLE ][ and APPLE ][ PLUS computers"
   homepage "http://mosher.mine.nu/epple2"
-  url "https://github.com/cmosher01/Epple-II.git", :revision => "130d2b4ac4333062f30d0e6ae5b668228e3cc36f"
-  version "130d2b4ac"
+  url "https://github.com/cmosher01/Epple-II.git",
+       :revision => "c50307a571b27f6de241fb609f94439c455bcd9b"
+  version "c50307a5"
   head "https://github.com/cmosher01/Epple-II.git"
 
+  depends_on "apple-ii-disk-tools"
   depends_on "automake" => :build
   depends_on "autoconf" => :build
   depends_on "sdl2"
-  # For epple2sys:
+  # For apple2sys:
   depends_on "xa" => :build
 
   needs :cxx11
 
-  resource "epple2sys" do
-    url "http://mosher.mine.nu/epple2/download/epple2sys-latest.tar.gz"
+  resource "apple2sys" do
+    url "http://mosher.mine.nu/apple2/download/apple2sys-latest.tar.gz"
     version "1.1"
-    sha256 "653c89bde1c4093d625d8b9fe6b8b008b43ccbe6160d92a02965153508450638"
+    sha256 "0fa87ae28035e3e1ec1d686e277e67e5b27b34cea5e2875a255f5a0e8f705197"
   end
 
   def install
     ENV.cxx11
     ENV.append "CXXFLAGS", "-DETCDIR=\\\"#{etc}\\\""
 
-    resource("epple2sys").stage do
+    resource("apple2sys").stage do
       system "./configure", "--prefix=#{prefix}"
       system "make", "install"
     end
