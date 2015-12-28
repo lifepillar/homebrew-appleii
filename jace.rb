e7a6ce38f4b963fdbef8d5ba7fb5494eafdd4824
Lifepillar <github@lifepillar.com>
2015-12-28 22:13:32 +0200

    Patch Jace source to rename config file.

--- jace.rb
+++ jace.rb
@@ -9,15 +9,36 @@ class Jace < Formula
   depends_on :java => "1.6+"
   depends_on "maven" => :build
 
+  # Rename .jace.conf => jace.conf
+  patch :DATA
 
   def install
     system "mvn", "package"
     libexec.install "target/jfx/app/jace-2.0-SNAPSHOT-jfx.jar"
     bin.write_jar_script libexec/"jace-2.0-SNAPSHOT-jfx.jar", "jace", "-Duser.dir=#{etc}"
   end
+
+  def caveats; <<-EOS.undent
+    Jace configuration is saved in
+        #{etc}/jace.conf
+    EOS
   end
 
   test do
     false
   end
 end
+
+__END__
+index e60459d..4a6e375 100644
+--- a/src/main/java/jace/config/Configuration.java
++++ b/src/main/java/jace/config/Configuration.java
+@@ -441,7 +441,7 @@ public class Configuration implements Reconfigurable {
+     }
+
+     public static File getSettingsFile() {
+-        return new File(System.getProperty("user.dir"), ".jace.conf");
++        return new File(System.getProperty("user.dir"), "jace.conf");
+     }
+
+     /**
