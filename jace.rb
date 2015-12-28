4e8a43c5d2ace39d33effc45b26eb2eddc0f6a12
Lifepillar <github@lifepillar.com>
2015-12-28 22:12:50 +0200

    Update formula: Jace.
    Build from source using Maven instead of downloading a pre-built
    package, because the link to the pre-built package is broken.
    
    Installing head is recommended.
    
    See also:
    
        https://github.com/badvision/jace/issues/1
        https://github.com/badvision/jace/issues/2

--- jace.rb
+++ jace.rb
@@ -1,17 +1,23 @@
 class Jace < Formula
   desc "Logical reproduction of an enhanced Apple //e computer"
   homepage "https://sites.google.com/site/brendanrobert/projects/jace"
-  url "https://downloads.sourceforge.net/project/java-ace/Builds/jace.jar"
-  version "2013-04-07"
-  sha256 "3f6d0227f46f7a8cacce5e8acdc57aca8cc09803fe680ff6f4aced62d3971754"
+  url "https://github.com/badvision/jace/archive/v2.0b.tar.gz"
+  version "2.0b"
+  sha256 "5ca3e38a9af710f259fd11a9b482a59b1591669915d2de8c93451d22f6dc3bff"
+  head "https://github.com/badvision/jace.git"
 
   depends_on :java => "1.6+"
+  depends_on "maven" => :build
+
 
   def install
-    libexec.install "jace.jar"
-    bin.write_jar_script libexec/"jace.jar", "jace", "-Duser.dir=$HOME"
+    system "mvn", "package"
+    libexec.install "target/jfx/app/jace-2.0-SNAPSHOT-jfx.jar"
+    bin.write_jar_script libexec/"jace-2.0-SNAPSHOT-jfx.jar", "jace", "-Duser.dir=#{etc}"
+  end
   end
 
   test do
+    false
   end
 end
