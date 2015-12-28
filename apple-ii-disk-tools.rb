38e90728b0e029574f42e0d017fd80a68c15a396
Lifepillar <github@lifepillar.com>
2015-12-28 14:44:46 +0200

    Update formula: Apple II Disk Tools.
    We download the stable version (1.1) from
    
        http://mosher.mine.nu/apple2/site/
    
    rather than Github because the release package on Github has some
    missing files (src/ctest is empty).

--- apple-ii-disk-tools.rb
+++ apple-ii-disk-tools.rb
@@ -1,6 +1,12 @@
 class AppleIiDiskTools < Formula
   desc "Utilities for Apple ][ floppy disk images"
   homepage "https://github.com/cmosher01/Apple-II-Disk-Tools"
+  # In the following archive, src/ctest is mistakenly empty:
+  # url "https://github.com/cmosher01/Apple-II-Disk-Tools/archive/1.1.tar.gz"
+  # sha256 "fc84650bdd7f2f7d7a7f40914ec4b8e7bacb15377f70bf9ea0b1e2f60a16e7e2"
+  url "http://mosher.mine.nu/apple2/download/a2catalog-latest.tar.gz"
+  version "1.1"
+  sha256 "798e6fcdcf7add395f9e741f9afc2d323e10d4d839a6d4cb2b09c101058afc57"
   head "https://github.com/cmosher01/Apple-II-Disk-Tools.git"
 
   depends_on "autoconf" => :build
