435d6ac1e720acc327c2b7cc73b7a30c4972e82f
Lifepillar <github@lifepillar.com>
2016-02-05 22:44:35 +0100

    New formula: fhpack.

---
+++ fhpack.rb
@@ -0,0 +1,16 @@
+class Fhpack < Formula
+  desc "Compression for Apple II hi-res images"
+  homepage "https://github.com/fadden/fhpack"
+  url "https://github.com/fadden/fhpack.git",
+    :revision => "8a83b45203e346c975be25d505013c8033655b94"
+  version "8a83b452"
+
+  def install
+    system ENV.cxx, "-O2", "-o", "fhpack", "fhpack.cpp"
+    bin.install "fhpack"
+  end
+
+  test do
+    system "fhpack"
+  end
+end
