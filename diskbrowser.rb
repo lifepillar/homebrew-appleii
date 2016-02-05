ef189ddf8e0cb3901a262b0cf75698602f3ef979
Lifepillar <github@lifepillar.com>
2016-02-05 22:05:50 +0100

    New formula: DiskBrowser 1.0.1.
    An Apple II Disk Browser written in Java.

---
+++ diskbrowser.rb
@@ -0,0 +1,18 @@
+class Diskbrowser < Formula
+  desc "Apple II Disk Browser"
+  homepage "https://github.com/dmolony/DiskBrowser"
+  url "https://github.com/dmolony/DiskBrowser/releases/download/v1.0.1/DiskBrowser.jar"
+  version "1.0.1"
+  sha256 "55b88a05ac470fbaea479b7489be73663a939cc2943d0cbafe98bb87ff5e7181"
+
+  depends_on :java => "1.8+"
+
+  def install
+    libexec.install "DiskBrowser.jar"
+    bin.write_jar_script libexec/"DiskBrowser.jar", "diskbrowser"
+  end
+
+  test do
+    system "false"
+  end
+end
