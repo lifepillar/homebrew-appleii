b79442f741a38c6117752a0f1e3b8e3199e8be36
Lifepillar <github@lifepillar.com>
2016-02-13 22:58:31 +0100

    Update Jace to version 2.0.
    Install the jar of the stable version instead of building from source.
    This removes a dependency (Maven is needed only for HEAD) and installs
    faster.

--- jace.rb
+++ jace.rb
@@ -1,19 +1,22 @@
 class Jace < Formula
   desc "Logical reproduction of an enhanced Apple //e computer"
   homepage "https://sites.google.com/site/brendanrobert/projects/jace"
-  url "https://github.com/badvision/jace/archive/v2.0b.tar.gz"
-  version "2.0b"
-  sha256 "5ca3e38a9af710f259fd11a9b482a59b1591669915d2de8c93451d22f6dc3bff"
+  url "https://github.com/badvision/jace/releases/download/2.0-stable/Jace.jar"
+  version "2.0"
+  sha256 "169df9846dfd496b985c0635717cbb1b93d2e4421d3662640ce32b2737ff6395"
   head "https://github.com/badvision/jace.git"
 
   depends_on :java => "1.8+"
-  depends_on "maven" => :build
-
+  depends_on "maven" => :build if build.head?
 
   def install
-    system "mvn", "package"
-    libexec.install "target/jfx/app/jace-2.0-SNAPSHOT-jfx.jar"
-    bin.write_jar_script libexec/"jace-2.0-SNAPSHOT-jfx.jar", "jace", "-Duser.dir=#{etc}"
+    if head?
+      system "mvn", "package"
+      libexec.install "target/Jace.jar"
+    else
+      libexec.install "Jace.jar"
+    end
+    bin.write_jar_script libexec/"Jace.jar", "jace", "-Duser.dir=#{etc}"
   end
 
   def caveats; <<-EOS.undent
