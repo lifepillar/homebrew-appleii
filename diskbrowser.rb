cd598e013bb651ca7217a6de0f520aead94caada
Lifepillar <github@lifepillar.com>
2016-02-19 09:34:23 +0100

    Fix DiskBrowser' version in download URL.

--- diskbrowser.rb
+++ diskbrowser.rb
@@ -1,7 +1,7 @@
 class Diskbrowser < Formula
   desc "Apple II Disk Browser"
   homepage "https://github.com/dmolony/DiskBrowser"
-  url "https://github.com/dmolony/DiskBrowser/releases/download/v1.0.1/DiskBrowser.jar"
+  url "https://github.com/dmolony/DiskBrowser/releases/download/v1.0.2/DiskBrowser.jar"
   version "1.0.2"
   sha256 "d1d35a7977d68a6658c28ca75e302a9bc242c07ff0b18401f5383f490a00e155"
 
