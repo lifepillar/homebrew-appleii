c21ea1629b07c800cae20b565608e7f0d34f1821
Lifepillar <github@lifepillar.com>
2016-08-02 21:23:16 +0300

    New formula: LLVM-6502.

---
+++ llvm-6502.rb
@@ -0,0 +1,27 @@
+class Llvm6502 < Formula
+  desc "LLVM backend for 6502"
+  homepage "https://github.com/c64scene-ar/llvm-6502"
+  url "https://github.com/c64scene-ar/llvm-6502.git",
+    :branch => "master",
+    :revision => "14ca03a76a7c034e6b71343a8cb2986ed594"
+  head "https://github.com/c64scene-ar/llvm-6502.git"
+
+  depends_on "libffi" => :recommended # http://llvm.org/docs/GettingStarted.html#requirement
+  if MacOS.version <= :snow_leopard
+    depends_on :python
+  else
+    depends_on :python => :optional
+  end
+  depends_on "cmake" => :build
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", "-DLLVM_TARGETS_TO_BUILD:STRING='X86;Mos6502'", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "false"
+  end
+end
