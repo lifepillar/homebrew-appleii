fc83eae8decbf553261f8815d936fe60afbac751
Lifepillar <github@lifepillar.com>
2016-08-10 22:34:39 +0300

    Add --with-gui option to AppleCommander formula.
    The GUI is not very functional, but anyway…

--- applecommander.rb
+++ applecommander.rb
@@ -5,16 +5,34 @@ class Applecommander < Formula
   url "https://sites.google.com/site/drjohnbmatthews/applecommander/AppleCommander-1.3.5.14-ac.jar"
   sha256 "84077fd4990398a6212a3ac2450c15979c071497c34a680666c9e24db5b4330b"
 
+  option "with-gui", "Additionally install the GUI."
+
   depends_on :java => "1.6+" # Not tested with previous versions
+
+  if build.with? "gui"
+    resource "AppleCommander-GUI" do
+      url "https://sites.google.com/site/drjohnbmatthews/applecommander/AppleCommander-1.3.5.14.jar"
+      sha256 "103531d7d06a9d607aebb0a2abc0e0e72ee26313e9ec0110f6d0ad210f8fba46"
+    end
+  end
 
   def install
     libexec.install "AppleCommander-1.3.5.14-ac.jar"
     bin.write_jar_script libexec/"AppleCommander-1.3.5.14-ac.jar", "applecommander"
+    if build.with? "gui"
+      resource("AppleCommander-GUI").stage do
+        libexec.install "AppleCommander-1.3.5.14.jar"
+        bin.write_jar_script libexec/"AppleCommander-1.3.5.14.jar", "applecommander-gui"
+      end
+    end
   end
 
-  def caveats; <<-EOS.undent
-    The command-line executable is called `applecommander`.
-    EOS
+  def caveats
+    s = "The command-line executable is called `applecommander`."
+    if build.with? "gui"
+      s += "\nTo run the GUI version use `applecommander-gui`."
+    end
+    s
   end
 
   test do
