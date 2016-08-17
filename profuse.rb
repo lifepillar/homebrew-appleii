1432b34f71db8a11317831e5b0828e68e1240396
Lifepillar <github@lifepillar.com>
2016-08-17 07:22:46 +0300

    Remove inreplace instructions from profuse formula.
    Include headers have been fixed upstream
    (https://github.com/ksherlock/profuse/issues/1).

--- profuse.rb
+++ profuse.rb
@@ -6,12 +6,6 @@ class Profuse < Formula
   depends_on :osxfuse
 
   def install
-    inreplace "bin/fuse_pascal.cpp",     "<fuse/fuse_opt.h>", "<fuse_opt.h>"
-    inreplace "bin/fuse_pascal.cpp",     "<fuse/fuse_lowlevel.h>", "<fuse_lowlevel.h>"
-    inreplace "bin/fuse_pascal_ops.cpp", "<fuse/fuse_opt.h>", "<fuse_opt.h>"
-    inreplace "bin/fuse_pascal_ops.cpp", "<fuse/fuse_lowlevel.h>", "<fuse_lowlevel.h>"
-    inreplace "bin/profuse.h",           "<fuse/fuse_opt.h>", "<fuse_opt.h>"
-    inreplace "bin/profuse.h",           "<fuse/fuse_lowlevel.h>", "<fuse_lowlevel.h>"
     system "make"
     # Do not install xattr, as it's already in OS X
     bin.install "o/apfm", "o/fuse_pascal", "o/newfs_pascal", "o/profuse"
