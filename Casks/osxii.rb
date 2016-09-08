2e62a4a0fffa2b9914f1ff2fed5d61bd1fa4150c
Lifepillar <github@lifepillar.com>
2016-09-08 22:04:41 +0200

    New cask: OSXII v0.9.

---
+++ Casks/osxii.rb
@@ -0,0 +1,19 @@
+cask 'osxii' do
+  version '0.9'
+  sha256 '9e0a92f5df117d3dd9b001e827fa4485bd6753190465b755fec430ef99857796'
+
+  url 'http://www.zophar.net/fileuploads/2/12325fpoak/OSXII_v09.dmg.gz'
+  name 'OSXII'
+  homepage 'http://www.zophar.net/macintosh/apple2/osxii.html'
+  license :gratis
+
+  app 'OSXII 0.9 Folder'
+
+  caveats do <<-EOS.undent
+    Read Roms.rtfd in "OSXII 0.9 Folder/Roms" directory.
+    Place Apple.ROM and Disk.ROM in "Roms" directory.
+    Place disk image files in "Disks" directory.
+  EOS
+  end
+end
+
