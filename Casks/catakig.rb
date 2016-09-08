270e9782026fd79116baaa61220223dd6d6ec399
Lifepillar <github@lifepillar.com>
2016-09-08 22:20:24 +0200

    New cask: Catakig v2.00b4.
    NOTE: the app starts up, but I am not sure that it recognizes ROM
    images.

---
+++ Casks/catakig.rb
@@ -0,0 +1,17 @@
+cask 'catakig' do
+  version '2.00b4'
+  sha256 '66be080bfb72b1489a528f6c4e67fbdc14c134a8a6056f1fe5a369cd97364612'
+
+  url 'http://www.zophar.net/fileuploads/2/12323nkscx/Catakig-2.00b4.dmg.zip'
+  name 'Catakig'
+  homepage 'http://www.zophar.net/macintosh/apple2/catakig.html'
+  license :gratis
+
+  app 'Catakig'
+
+  caveats do
+    <<-EOS.undent
+    ROMs files must be put in /Applications/Catakig/ROMs.
+    EOS
+  end
+end
