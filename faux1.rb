048549eab79a3505c0ac2b6164cb6f21e56dabab
Lifepillar <github@lifepillar.com>
2016-09-09 18:08:32 +0200

    New formula: faux1 v1.0.95.9.20120222.

---
+++ faux1.rb
@@ -0,0 +1,30 @@
+class Faux1 < Formula
+  desc "Apple I emulator for the terminal with cut and paste"
+  homepage "https://sourceforge.net/projects/faux1"
+  url "https://downloads.sourceforge.net/project/faux1/faux1.0.95.9.20120222.src.tar.gz"
+  version "1.0.95.9.20120222"
+  sha256 "fc5c72085abd0e5925d57c477d5e4dc20ded83723a03bd3c5f09281cdc43f25e"
+
+  def install
+    cd "src" do
+      system "make"
+    end
+    libexec.install "apps"
+    libexec.install "rom"
+    libexec.install "faux1"
+    (bin/"faux1").write <<-eos.undent
+        #!/bin/bash
+        cd #{libexec} && exec "./faux1" "$@"
+    eos
+  end
+
+  def caveats; <<-EOS.undent
+    When faux1 is running, the working directory is set to
+    #{libexec}
+    EOS
+  end
+
+  test do
+    false
+  end
+end
