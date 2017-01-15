4507e5018e3156f3b8afcfa0aee995c760693788
Lifepillar <lifepillar@lifepillar.me>
2017-01-15 12:24:30 +0100

    Update DiskBrowser to v1.0.16.

--- diskbrowser.rb
+++ diskbrowser.rb
@@ -1,9 +1,9 @@
 class Diskbrowser < Formula
   desc "Apple II Disk Browser"
   homepage "https://github.com/dmolony/DiskBrowser"
-  url "https://github.com/dmolony/DiskBrowser/releases/download/v1.0.15/DiskBrowser.jar"
-  version "1.0.15"
-  sha256 "e5ace60ca1888d63765ab2494f4897499d0241228810d5f1ec25cd593075607b"
+  url "https://github.com/dmolony/DiskBrowser/releases/download/v1.0.16/DiskBrowser.jar"
+  version "1.0.16"
+  sha256 "a54459a3261cb45b1ea91eb81dcbb34a88e677079bb3052abafd3d25f974ed80"
 
   depends_on :java => "1.6+"
 
