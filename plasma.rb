10c22f77f2157d8422075b2dd5ae9358b4ccb6e4
Lifepillar <lifepillar@lifepillar.me>
2017-01-22 19:08:59 +0100

    Set revision for PLASMA formula.
    There is a regression in current HEAD, so we build the last working
    revision by default. You may still build the current master with --HEAD.

--- plasma.rb
+++ plasma.rb
@@ -1,6 +1,9 @@
 class Plasma < Formula
   desc "Proto Language AsSeMbler for Apple"
   homepage "https://github.com/dschmenk/PLASMA"
+  url "https://github.com/dschmenk/PLASMA.git",
+      :revision => "a5215e330905e8ecc026dcddcb5afb24b54f4efa"
+  version "a5215e33"
   head "https://github.com/dschmenk/PLASMA.git"
 
   option "with-test", "Verify the build with `make test`"
