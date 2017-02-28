21992d9f7f177d34cff9c50fc164eb910e3abcdd
Lifepillar <lifepillar@lifepillar.me>
2017-02-28 20:04:07 +0100

    New cask: Buckshot v0.03.

---
+++ Casks/buckshot.rb
@@ -0,0 +1,11 @@
+cask 'buckshot' do
+  version '0.03'
+  sha256 '08e577f253f7fad5b2d471f9289de8289de3e0fe05de66367dd09ddb352471fb'
+
+  url 'https://apple2.gs/downloads/buckshot/0.03/buckshot.dmg'
+  name 'buckshot'
+  homepage 'https://apple2.gs/buckshot/'
+  license :gratis
+
+  app 'buckshot.app'
+end
