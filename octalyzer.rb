4ce34f28080e267672d26c547b5b55c2e63a5a8b
Lifepillar <lifepillar@lifepillar.me>
2017-05-11 08:27:07 +0200

    Update octalyzer formula.

--- octalyzer.rb
+++ octalyzer.rb
@@ -2,8 +2,8 @@ class Octalyzer < Formula
   desc "Apple //e Emulator for Millennials!"
   homepage "http://octalyzer.com/"
   url "http://yewsi.com/octalyzer/octalyzer-mac.zip"
-  version "0.0.1alpha-201703191211"
-  sha256 "36b9f530239c8b5dd70f81c56ccd3a15539a70f6fbe67945f8f605beb87fbfd5"
+  version "2017-03-27"
+  sha256 "61eaae09738af5df08be897a877cb723edafc05473ab9e5fd7952f09447929a3"
 
   def install
     bin.install "octalyzer"
