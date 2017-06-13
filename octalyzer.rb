1ea11c60766b023aa6e0547f66a544fc21309f94
Lifepillar <lifepillar@lifepillar.me>
2017-06-13 09:46:53 +0200

    Update The Octalyzer to v0.0.1alpha-201706120217.

--- octalyzer.rb
+++ octalyzer.rb
@@ -2,8 +2,8 @@ class Octalyzer < Formula
   desc "Apple //e Emulator for Millennials!"
   homepage "http://octalyzer.com/"
   url "http://yewsi.com/octalyzer/octalyzer-mac.zip"
-  version "2017-05-15"
-  sha256 "9c81f9f21f1f59d719be0e312b475b8bda3d4ff0764eba6cc84bb5274e01867a"
+  version "0.0.1alpha-201706120217"
+  sha256 "6ced69de88e79f59033ec9f824abb7fe6e55a11d82bf39bbb88a0ee254f32c1f"
 
   def install
     bin.install "octalyzer"
