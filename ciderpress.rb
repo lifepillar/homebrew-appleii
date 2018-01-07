3ecd0ef7db78cc18adc8ae2b37ebbf79944ff559
Lifepillar <lifepillar@lifepillar.me>
2018-01-07 11:19:31 +0200

    New formula: Ciderpress 4.0.3.

---
+++ ciderpress.rb
@@ -0,0 +1,37 @@
+class Ciderpress < Formula
+  desc "Apple II archive utility for Windows"
+  homepage "http://a2ciderpress.com"
+  url "https://github.com/fadden/ciderpress/releases/download/v4.0.3/CiderPress403.zip"
+  version "4.0.3"
+  sha256 "fc21b46adbd01ac5911066d99ef3b74cb6704bd8df234e724a183ad451e7cd25"
+
+  bottle :unneeded
+
+  depends_on "wine"
+
+  def install
+    libexec.install Dir["*"]
+
+    bin.mkpath
+    (bin/"ciderpress").write <<-EOS.undent
+      #!/bin/bash
+      wine ${HOME}'/.wine/drive_c/Program Files (x86)/faddenSoft/CiderPress/CiderPress.exe' "$@"
+    EOS
+  end
+
+  def caveats; <<-EOS.undent
+    To complete the installation execute:
+
+      cd #{libexec}
+      wine Setup403.exe
+
+    and choose No-Question-Asked Installation.
+
+    Ciderpress can then be launched from the command-line with `ciderpress`.
+    EOS
+  end
+
+  test do
+    system "false"
+  end
+end
