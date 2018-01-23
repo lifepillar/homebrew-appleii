e1017fb3a8f7af9ffa7db5d902f49a3f9ad90aef
Lifepillar <lifepillar@lifepillar.me>
2018-01-23 08:38:07 +0100

    Update doss3fsprogs to tfv004.

--- dos33fsprogs.rb
+++ dos33fsprogs.rb
@@ -1,17 +1,14 @@
 class Dos33fsprogs < Formula
   desc "Apple DOS 3.3 filesystem tools"
   homepage "http://www.deater.net/weave/vmwprod/apple/dos33fs.html"
-  url "http://www.deater.net/weave/vmwprod/apple/dos33fsprogs-0.0.10.tar.gz"
-  sha256 "b7bcaddda5f403621c6551eb6ad67900e9c7fcd11b78c4ceced009a08725dd5a"
+  url "https://github.com/deater/dos33fsprogs/archive/tfv004.tar.gz"
+  version "tfv004"
+  sha256 "8bcc36e6d7fca9319fca085783a9aa13606a19a67d5ec643aa4c5c902af7c4f1"
   head "https://github.com/deater/dos33fsprogs.git"
 
   def install
     mkdir_p bin
-    if head?
-      inreplace "Makefile.inc", "/usr/local/bin", bin
-    else
-      inreplace "Makefile", "/usr/local/bin", bin
-    end
+    inreplace "Makefile.inc", "/usr/local/bin", bin
     system "make"
     system "make", "install"
   end
