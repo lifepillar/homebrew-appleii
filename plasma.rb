ae29773611a457c5536c42e5f70f1d049fcb8169
Lifepillar <lifepillar@lifepillar.me>
2018-02-07 10:07:36 +0100

    Update PLASMA to commit 7f9975c4.
    This is v1.0, but it has not been tagged by the author.
    
    For some reason, Homebrew chokes on make with this release. The
    workaround I have found consists in runnning make twice, ignoring the
    error of the first run. I have no problems building PLASMA when I run
    make manually.

--- plasma.rb
+++ plasma.rb
@@ -2,8 +2,8 @@ class Plasma < Formula
   desc "Proto Language AsSeMbler for Apple"
   homepage "https://github.com/dschmenk/PLASMA"
   url "https://github.com/dschmenk/PLASMA.git",
-      :revision => "a5215e330905e8ecc026dcddcb5afb24b54f4efa"
-  version "a5215e33"
+      :revision => "7f9975c4429e07e5687b947854ded7922f0561d5"
+  version "7f9975c4"
   head "https://github.com/dschmenk/PLASMA.git"
 
   option "with-test", "Verify the build with `make test`"
@@ -12,6 +12,10 @@ class Plasma < Formula
 
   def install
     cd "src" do
+      begin # For some reason, the first make fails, but second builds correctly
+        system "make"
+      rescue
+      end
       system "make"
       system "make", "test" if build.with?("test")
     end
