c950a863274b638e0f86da791839dde0893ee13f
Mike Finger <mike.finger@species360.org>
2018-04-04 21:10:11 -0500

    Renamed forumla and fixed issues

--- mach-kernel-cadius.rb
+++ mach-kernel-cadius.rb
@@ -1,8 +1,7 @@
-class Cadius < Formula
-  desc "A maintained fork of BrutalDeluxe's Cadius ProDOS disk imaging utility (used for making Apple II disk images)."
-  homepage "https://github.com/mach-kernel/cadius"
+class MachKernelCadius < Formula
+  desc "BrutalDeluxe's Cadius ProDOS disk imaging utility"
+  homepage "https://www.brutaldeluxe.fr/products/crossdevtools/cadius"
   url "https://github.com/mach-kernel/cadius/archive/1.4.0.tar.gz"
-  version "1.4.0"
   sha256 "6755f73a45f879c361d0eacb923f675c66038699d9cfb4856ac6814832defc96"
 
   def install
