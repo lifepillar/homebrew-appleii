ade6fb35d1809450e49481f3f06ec0a1235b3c27
Lifepillar <lifepillar@lifepillar.me>
2018-04-30 08:29:19 +0200

    Update OpenEmulator cask to v1.0.3.1-20180428.

--- Casks/openemulator.rb
+++ Casks/openemulator.rb
@@ -1,8 +1,8 @@
 cask 'openemulator' do
-  version '1.0.3.1-20170517'
-  sha256 '53afa43a485834b1326983f9f02f613206086c22fa5ba10a1951e51ca04c0e3e'
+  version '1.0.3.1-20180428'
+  sha256 '0b82f387a67e9ead66e8cd22e7539ac60ed148c1125543cafa6313807d1f6516'
 
-  url 'https://archive.org/download/OpenEmulatorSnapshots/OpenEmulator-20170517.zip'
+  url 'https://archive.org/download/OpenEmulatorSnapshots/OpenEmulator-20180428.zip'
   name 'OpenEmulator'
   name 'Accurate, portable emulator of legacy computer systems'
   homepage 'https://archive.org/details/OpenEmulatorSnapshots'
