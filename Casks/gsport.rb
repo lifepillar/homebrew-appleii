e3818ceba7fe9540e7f71330cd3520fd7fef04e0
Lifepillar <lifepillar@lifepillar.me>
2018-06-24 20:04:14 +0200

    Update homepage and download URL for GSPort.

--- Casks/gsport.rb
+++ Casks/gsport.rb
@@ -2,10 +2,11 @@ cask 'gsport' do
   version '0.31'
   sha256 '00d5df71ff0ee6932ece28b248e100fdb378d05d958374223c15962b2529b913'
 
-  url 'http://downloads.sourceforge.net/sourceforge/gsport/gsport_0.31.dmg'
+  url "https://github.com/david-schmidt/gsport/releases/download/#{version}/gsport_#{version}.dmg"
+  appcast 'https://github.com/david-schmidt/gsport/releases.atom'
   name 'GSPort'
   name 'Portable Apple IIGS emulator'
-  homepage 'http://gsport.sourceforge.net'
+  homepage 'https://david-schmidt.github.io/gsport/'
 
   app 'GSPort/GSPort.app'
 
