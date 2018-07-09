d360dd21c2442bc159d2cced7bd7c7fb5a14a63a
Lifepillar <lifepillar@lifepillar.me>
2018-07-09 22:56:29 +0200

    Update Cadius (mach-kernel's fork) to v1.4.2.

--- Formula/mach-kernel-cadius.rb
+++ Formula/mach-kernel-cadius.rb
@@ -1,8 +1,8 @@
 class MachKernelCadius < Formula
   desc "BrutalDeluxe's Cadius ProDOS disk imaging utility"
   homepage "https://www.brutaldeluxe.fr/products/crossdevtools/cadius"
-  url "https://github.com/mach-kernel/cadius/archive/1.4.0.tar.gz"
-  sha256 "6755f73a45f879c361d0eacb923f675c66038699d9cfb4856ac6814832defc96"
+  url "https://github.com/mach-kernel/cadius/archive/1.4.2.tar.gz"
+  sha256 "1360d51fe7f1f9017420d83a3d2a2bee0cd1d60a75033dfb76db13d5d081d5c5"
 
   def install
     system "make"
@@ -10,6 +10,6 @@ class MachKernelCadius < Formula
   end
 
   test do
-    system "cadius"
+    system "#{bin}/cadius"
   end
 end
