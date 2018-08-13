05c393bb2555e04ec0a8034298da0c0773185925
Lifepillar <lifepillar@lifepillar.me>
2018-08-13 15:37:52 +0300

    Update Clock Signal to version 2018-08-05.

--- Casks/clock-signal.rb
+++ Casks/clock-signal.rb
@@ -1,10 +1,8 @@
 cask 'clock-signal' do
-  version '2018-06-24'
-  sha256 '2bf60cd841e7755466652a2abc5e47d13028979bff539656b182e578b57575c5'
+  version '2018-08-05'
+  sha256 '31557511f75dfd51379e4b04d40fe6f21ec585ad4f80538490fd5c8997e6dec5'
 
-  # url "https://github.com/TomHarte/CLK/releases/download/#{version}/Clock.Signal.macOS.#{version}.zip"
-  # Release 2018-06-24 has a different pattern for the URL:
-  url "https://github.com/TomHarte/CLK/releases/download/untagged-83a648386d4da3db578a/Clock.Signal.macOS.#{version}.zip"
+  url "https://github.com/TomHarte/CLK/releases/download/#{version}/Clock.Signal.macOS.#{version}.zip"
   appcast 'https://github.com/TomHarte/CLK/releases.atom'
   name 'Clock Signal'
   homepage 'https://github.com/TomHarte/CLK'
