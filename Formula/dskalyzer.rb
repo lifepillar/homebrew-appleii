25479e72f7eeb11cb8fe0d11395bcb166a97e27e
Lifepillar <lifepillar@lifepillar.me>
2018-10-27 10:22:33 +0200

    Fix home URL of dskalyzer.

--- Formula/dskalyzer.rb
+++ Formula/dskalyzer.rb
@@ -1,6 +1,6 @@
 class Dskalyzer < Formula
   desc "Apple II disk image management tool"
-  homepage "http://octalyzer.com/dskalyzer/"
+  homepage "https://github.com/paleotronic/dskalyzer"
   url "https://github.com/paleotronic/dskalyzer/archive/v0.1.3.tar.gz"
   sha256 "6e150746eb62bfa80b8edc513cd786f6a7917b837fe1ecf199eac132ff6b9d03"
   head "https://github.com/paleotronic/dskalyzer.git"
