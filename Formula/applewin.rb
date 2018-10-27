045e45bdf7aaf13bafbb9fba4220bcc4e2569fef
Lifepillar <lifepillar@lifepillar.me>
2018-10-27 10:06:04 +0200

    Update AppleWin to v1.27.9.0.

--- Formula/applewin.rb
+++ Formula/applewin.rb
@@ -1,8 +1,8 @@
 class Applewin < Formula
   desc "Apple II emulator for Windows"
   homepage "https://github.com/AppleWin/AppleWin"
-  url "https://github.com/AppleWin/AppleWin/releases/download/v1.27.2.0/AppleWin1.27.2.0.zip"
-  sha256 "cb4685ead6f4708f9c7fccf760b3435133d5f26a747934582c919dea5f76ddfd"
+  url "https://github.com/AppleWin/AppleWin/releases/download/v1.27.9.0/AppleWin1.27.9.0.zip"
+  sha256 "b5146b858eb94d298490a3943d979dc8144c063d953df0ed5a2e87f2ce3aea04"
 
   bottle :unneeded
 
