91883e4c05aab97902f958192af4982d25ee8353
Lifepillar <lifepillar@lifepillar.me>
2019-01-26 20:20:12 +0100

    Update cask: adtpro 2.0.3

--- Casks/adtpro.rb
+++ Casks/adtpro.rb
@@ -1,13 +1,13 @@
 cask 'adtpro' do
-  version '2.0.2'
-  sha256 '29e711906520172684e363ec4e7b55d09d915c0cf22444df4403595d28f47ccf'
+  version "2.0.3"
+  sha256 "fb3d380a1a5196a950a2a90711fa99e2bd6bfd025891984e5ff967b2725d5dfc"
 
   adtpro = "ADTPro-#{version}"
   url "https://github.com/ADTPro/adtpro/releases/download/v_#{version.tr('.','_')}/#{adtpro}.dmg"
   appcast "https://github.com/ADTPro/adtpro/releases.atom"
-  name 'ADTPro'
-  name 'Apple Disk Transfer ProDOS'
-  homepage 'http://adtpro.com'
+  name "ADTPro"
+  name "Apple Disk Transfer ProDOS"
+  homepage "http://adtpro.com"
 
   # Copy the whole folder
   app "#{adtpro}"
