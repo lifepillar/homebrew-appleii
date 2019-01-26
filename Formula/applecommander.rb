a0bfbbcde9d9d39349f64f3f83ada65865ced8a4
Lifepillar <lifepillar@lifepillar.me>
2019-01-26 19:57:20 +0100

    Update formula: applecommander 1.5.0.

--- Formula/applecommander.rb
+++ Formula/applecommander.rb
@@ -2,7 +2,7 @@ class Applecommander < Formula
   desc "Move data between Apple ][ disk images and native filesystem"
   homepage "http://applecommander.github.io"
   url "https://github.com/AppleCommander/AppleCommander/releases/download/v1-5-0/AppleCommander-ac-1.5.0.jar"
-  sha256 "4994afa46b67b30dd74c1964caca94c8df1a6e8e9bad3e141ecb3a4519256eb2"
+  sha256 "31f9989dd69148b865f1b135833327b2ea2462cfe038f1bde7ed30f55d19d588"
 
   option "with-gui", "Additionally install the GUI."
 
@@ -10,18 +10,18 @@ class Applecommander < Formula
 
   if build.with? "gui"
     resource "AppleCommander-GUI" do
-      url "https://github.com/AppleCommander/AppleCommander/releases/download/v1-4-0/AppleCommander-macosx-1.4.0.jar"
-      sha256 "e35ce5676f8329da3f61e4a6be3dc9f5c0776524033889473db0c4c663e503be"
+      url "https://github.com/AppleCommander/AppleCommander/releases/download/v1-5-0/AppleCommander-macosx-1.5.0.jar"
+      sha256 "91661cf06d99736c66e2f23e921239a7754ed5527d29943d4928473a6ac869fc"
     end
   end
 
   def install
-    libexec.install "AppleCommander-ac-1.4.0.jar"
-    bin.write_jar_script libexec/"AppleCommander-ac-1.4.0.jar", "applecommander"
+    libexec.install "AppleCommander-ac-1.5.0.jar"
+    bin.write_jar_script libexec/"AppleCommander-ac-1.5.0.jar", "applecommander"
     if build.with? "gui"
       resource("AppleCommander-GUI").stage do
-        libexec.install "AppleCommander-macosx-1.4.0.jar"
-        bin.write_jar_script libexec/"AppleCommander-macosx-1.4.0.jar", "applecommander-gui", "-XstartOnFirstThread"
+        libexec.install "AppleCommander-macosx-1.5.0.jar"
+        bin.write_jar_script libexec/"AppleCommander-macosx-1.5.0.jar", "applecommander-gui", "-XstartOnFirstThread"
       end
     end
   end
