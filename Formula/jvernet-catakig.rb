94bd26ef2509bd8ce019272e8134f63204c5b00a
Lifepillar <lifepillar@lifepillar.me>
2019-01-26 21:05:26 +0100

    Update formula: catakig 20190126.
    This builds successfully, but it crashes at runtime in macOS Mojave.

--- Formula/jvernet-catakig.rb
+++ Formula/jvernet-catakig.rb
@@ -2,29 +2,25 @@ class JvernetCatakig < Formula
   desc "Fork of Catakig that builds on newer Xcode"
   homepage "https://github.com/jvernet/Catakig"
   url "https://github.com/jvernet/Catakig.git",
-    :branch => "master",
-    :revision => "dc676711b8128453b7a897ff9a9e5008c416e260"
-  version "dc676711"
+    :revision => "61f2551e236204b1fac5200e5ed0d8acbdc3b1e2"
+  version "20190126"
   head "https://github.com/jvernet/Catakig.git"
 
   def install
-    args = [
-      "-configuration", "Release",
+    xcodebuild "-configuration", "Release",
       "-alltargets",
       "MACOSX_DEPLOYMENT_TARGET=#{MacOS.version}",
       "SYMROOT=build",
       "PREFIX=#{prefix}",
-      "ARCHS=i386"
-    ]
+      "HEADER_SEARCH_PATHS=./Source/LibAppleII"
 
-    xcodebuild *args
     prefix.install "build/Release/Catakig.app"
   end
 
   def caveats; <<~EOS
     Catakig.app has been installed in #{opt_prefix}.
     You may create an alias in your Applications folder if you want.
-    EOS
+  EOS
   end
 
   test do
