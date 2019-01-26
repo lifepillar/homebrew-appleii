fc55aeddbb1a5f3bd31b9c284c66256597e91eef
Lifepillar <lifepillar@lifepillar.me>
2019-01-26 20:34:28 +0100

    Update cask: microm8

--- Casks/microm8.rb
+++ Casks/microm8.rb
@@ -1,6 +1,6 @@
 cask 'microm8' do
   version '1.0.0'
-  sha256 '071dbbca9044fd1a60e612ca7e561f55e40dd506e3c0c96152d79af214591888'
+  sha256 '7c5286ef11f482bbb97db2f4f13c7fc7c3f990594853023556ef389bcfc0d71f'
 
   url "http://paleotronic.com/download/microm8-mac.zip"
   name 'MicroM8'
