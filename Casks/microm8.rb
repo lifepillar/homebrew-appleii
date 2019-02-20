c697ebfcd278f1b83babb985ed590f96839617ad
Lifepillar <lifepillar@lifepillar.me>
2019-02-20 09:15:39 +0100

    Update formula: microm8.

--- Casks/microm8.rb
+++ Casks/microm8.rb
@@ -1,6 +1,6 @@
 cask 'microm8' do
   version '1.0.0'
-  sha256 '7c5286ef11f482bbb97db2f4f13c7fc7c3f990594853023556ef389bcfc0d71f'
+  sha256 'efc44dd7e531d4ba5a6d3aceb5eb78ccbdc4a75c9f1227ce20de595be02eaafd'
 
   url "http://paleotronic.com/download/microm8-mac.zip"
   name 'MicroM8'
