83ff448cef2277af94fb2d27de0d69b5f8b53403
Rob Greene <robgreene@gmail.com>
2020-01-20 19:01:03 -0600

    Updating for AppleCommander 1.6.0.

--- Formula/applecommander.rb
+++ Formula/applecommander.rb
@@ -1,8 +1,8 @@
 class Applecommander < Formula
   desc "Move data between Apple ][ disk images and native filesystem"
   homepage "http://applecommander.github.io"
-  url "https://github.com/AppleCommander/AppleCommander/releases/download/v1-5-0/AppleCommander-ac-1.5.0.jar"
-  sha256 "31f9989dd69148b865f1b135833327b2ea2462cfe038f1bde7ed30f55d19d588"
+  url "https://github.com/AppleCommander/AppleCommander/releases/download/v1-6-0/AppleCommander-ac-1.6.0.jar"
+  sha256 "7a6a9128798d5f1affc51963f0ab1684de592e41bf960be4ab4b2e5d994e0df6"
 
   option "with-gui", "Additionally install the GUI."
 
@@ -10,18 +10,18 @@ class Applecommander < Formula
 
   if build.with? "gui"
     resource "AppleCommander-GUI" do
-      url "https://github.com/AppleCommander/AppleCommander/releases/download/v1-5-0/AppleCommander-macosx-1.5.0.jar"
-      sha256 "91661cf06d99736c66e2f23e921239a7754ed5527d29943d4928473a6ac869fc"
+      url "https://github.com/AppleCommander/AppleCommander/releases/download/v1-6-0/AppleCommander-macosx-1.6.0.jar"
+      sha256 "fe15adf8967644cb08304864990f93a06ecf159c31a0c470247b1cb081fdb82c"
     end
   end
 
   def install
-    libexec.install "AppleCommander-ac-1.5.0.jar"
-    bin.write_jar_script libexec/"AppleCommander-ac-1.5.0.jar", "applecommander"
+    libexec.install "AppleCommander-ac-1.6.0.jar"
+    bin.write_jar_script libexec/"AppleCommander-ac-1.6.0.jar", "applecommander"
     if build.with? "gui"
       resource("AppleCommander-GUI").stage do
-        libexec.install "AppleCommander-macosx-1.5.0.jar"
-        bin.write_jar_script libexec/"AppleCommander-macosx-1.5.0.jar", "applecommander-gui", "-XstartOnFirstThread"
+        libexec.install "AppleCommander-macosx-1.6.0.jar"
+        bin.write_jar_script libexec/"AppleCommander-macosx-1.6.0.jar", "applecommander-gui", "-XstartOnFirstThread"
       end
     end
   end
