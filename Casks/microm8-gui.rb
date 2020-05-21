0067f3bac2951abb036af2c28fb52f2ccd08722a
Lifepillar <lifepillar@lifepillar.me>
2020-05-21 12:35:07 +0200

    New cask: microm8-gui v1.1

---
+++ Casks/microm8-gui.rb
@@ -0,0 +1,12 @@
+cask 'microm8-gui' do
+  version '1.1'
+  sha256 '461af119f22eed3b0572f689308b7fd1dc028b8654931f56c5931377d6b115b4'
+
+  url "https://github.com/paleotronic/microm8-gui/releases/download/v1.1/microM8-gui-macos-x64.zip"
+  name 'microM8-gui'
+  homepage 'https://paleotronic.com/software/microm8/'
+
+  app 'microM8-gui.app'
+end
+
+
