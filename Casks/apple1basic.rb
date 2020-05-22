1f8b90b90baa4ca5862a7585d49b8467d66b2c13
Eric Knibbe <enk3@outlook.com>
2020-05-21 22:07:00 -0400

    New cask: apple1basic

---
+++ Casks/apple1basic.rb
@@ -0,0 +1,11 @@
+cask 'apple1basic' do
+  version '20080718' # Date of original post
+  sha256 'f697b2e8893f04064367c39fa83c08024b7e4ec427b2436a014a555ba4ba4550'
+
+  # www.weihenstephan.org/~michaste/pagetable/recompiler was verified as official when first introduced to the cask
+  url 'http://www.weihenstephan.org/~michaste/pagetable/recompiler/apple1basic_osx.zip'
+  name 'Apple I BASIC'
+  homepage 'https://www.pagetable.com/?p=35'
+
+  binary 'apple1basic'
+end
