a818f8b609c6f9010ab832a5fd9d2c9dcf8ebf13
Lifepillar <lifepillar@lifepillar.me>
2020-08-08 22:57:00 +0200

    New cask: Steve ][ v0.85-beta.1

---
+++ Casks/steve2.rb
@@ -0,0 +1,18 @@
+cask "steve2" do
+  version "0.85-beta.1"
+  sha256 "cdb4cb9f4d937a0211be78b6819130391d9b4a9f623be28dd1aaef91df197f42"
+
+  url "https://github.com/trudnai/Steve2/releases/download/v#{version}/Steve2.pkg"
+  appcast "https://github.com/trudnai/Steve2/releases.atom"
+  name "Steve ]["
+  homepage "https://github.com/trudnai/Steve2"
+
+  pkg "Steve2.pkg"
+
+  uninstall pkgutil: "com.trudnai.pkg.Steve2"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.trudnai.steveii",
+    "~/Library/Containers/com.trudnai.steveii",
+  ]
+end
