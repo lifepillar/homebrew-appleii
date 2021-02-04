efeb01ef804a5a689155beef33f2800e2b959226
Lifepillar <lifepillar@lifepillar.me>
2021-02-04 09:18:31 +0100

    Update cask: clock-signal (CLK) 2021-02-02

--- Casks/clock-signal.rb
+++ Casks/clock-signal.rb
@@ -1,8 +1,8 @@
 cask "clock-signal" do
-  version "2020-11-13"
-  sha256 "836fbd5ac6cfcdfdac378bd3b9c714bac7087d5f53ff2dfd85217d69013205c0"
+  version "2021-02-02"
+  sha256 "3e4b323900b6a28817c59a949082fc853e212c6281488009661e2a82e9bd40f9"
 
-  url "https://github.com/TomHarte/CLK/releases/download/#{version}/Clock.Signal.macOS.#{version}.updated.zip"
+  url "https://github.com/TomHarte/CLK/releases/download/#{version}/Clock.Signal.macOS.#{version}.zip"
   appcast "https://github.com/TomHarte/CLK/releases.atom"
   name "Clock Signal"
   homepage "https://github.com/TomHarte/CLK"
