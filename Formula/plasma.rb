06ab739778623c344d9c9325aba1632e8f67c6ac
Lifepillar <lifepillar@lifepillar.me>
2021-02-19 15:25:02 +0100

    Update formula: plasma 2.0 Alpha 1

--- Formula/plasma.rb
+++ Formula/plasma.rb
@@ -1,9 +1,9 @@
 class Plasma < Formula
   desc "Proto Language AsSeMbler for Apple"
   homepage "https://github.com/dschmenk/PLASMA"
-  url "https://github.com/dschmenk/PLASMA/archive/DP3.tar.gz"
-  version "2.0-DP3"
-  sha256 "d565fba316a5788b6270f8d7a666e163565aa67ed2c889f421891897ed885ce2"
+  url "https://github.com/dschmenk/PLASMA/archive/Alpha1.tar.gz"
+  version "2.0-alpha1"
+  sha256 "2a8f90cef2956d060861ad943204d7a5bb48b8b491ac18522002d1fae95e091d"
   head "https://github.com/dschmenk/PLASMA.git"
 
   option "with-test", "Verify the build with `make test`"
