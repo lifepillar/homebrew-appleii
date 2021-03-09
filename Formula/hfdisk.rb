53a04c788dccdc278ae5d5e6f4fd51048972e682
Lifepillar <lifepillar@lifepillar.me>
2021-03-09 16:57:45 +0100

    [hfdisk] Use HTTPS instead of HTTP.

--- Formula/hfdisk.rb
+++ Formula/hfdisk.rb
@@ -1,7 +1,7 @@
 class Hfdisk < Formula
   desc "Port of the Apple MkLinux mac-fdisk pdisk utility"
-  homepage "http://www.codesrc.com/gitweb/index.cgi?p=hfdisk.git;a=summary"
-  url "http://www.codesrc.com/gitweb/index.cgi?p=hfdisk.git;a=snapshot;h=a7659ed434abc8fb6994498e9e0e6e2c40bfbf51;sf=tgz"
+  homepage "https://www.codesrc.com/gitweb/index.cgi?p=hfdisk.git;a=summary"
+  url "https://www.codesrc.com/gitweb/index.cgi?p=hfdisk.git;a=snapshot;h=a7659ed434abc8fb6994498e9e0e6e2c40bfbf51;sf=tgz"
   version "1.0-rc3"
   sha256 "5e38c66169a1988441f9a7e4b68d692a4ea6450fe1fccb44b335788257163972"
   head "git://www.codesrc.com/git/hfdisk.git"
