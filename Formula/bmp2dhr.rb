a0ce1a32536d992ddda623a4b398389d71da2deb
Lifepillar <lifepillar@lifepillar.me>
2021-03-28 12:12:53 +0200

    Update formula: bmp2dhr

--- Formula/bmp2dhr.rb
+++ Formula/bmp2dhr.rb
@@ -1,16 +1,15 @@
 class Bmp2dhr < Formula
   desc "Convert modern graphics to Apple II graphics"
-  homepage "http://www.appleoldies.ca/bmp2dhr/"
-  url "http://www.appleoldies.ca/cc65/programs/dhgr/bmp2dhr.zip"
+  homepage "https://www.appleoldies.ca/bmp2dhr/"
+  url "https://www.appleoldies.ca/cc65/programs/dhgr/bmp2dhr.zip"
   version "20160824"
-  sha256 "4cd25bccddae411a512ef172b447083ca9d4eb72ffb58a5bdadd1e4200a8b0ac"
-
-  conflicts_with "digarok-bmp2dhr", because: "both install a `b2d` executable"
+  sha256 "a55ea2f0bedf9229f566a3bba7c60cf1c970e6e0f51f5fb7cfc52166e740e4ee"
 
   # To use the batch files provided with A2B to do automatic conversions
   depends_on "imagemagick"
 
-  # Fix random() function conflicting with the same function in stdlib.h
+  conflicts_with "digarok-bmp2dhr", because: "both install a `b2d` executable"
+
   patch :DATA
 
   def install
@@ -23,9 +22,6 @@ class Bmp2dhr < Formula
     cd "magick" do
       system ENV.cc, "-DMINGW", "-o", "m2s", "m2s.c"
     end
-    cd "xpack" do
-      system ENV.cc, "-DMINGW", "-o", "xpack", "xpack.c"
-    end
     libexec.install "bmp"
     libexec.install "dither"
     libexec.install "msk"
@@ -36,14 +32,14 @@ class Bmp2dhr < Formula
     bin.install "src/b2d"
     bin.install_symlink bin/"b2d" => "bmp2dhr"
     bin.install "magick/m2s"
-    bin.install "xpack/xpack"
   end
 
-  def caveats; <<~EOS
-    A template directory can be copied from #{libexec}/template
+  def caveats
+    <<~EOS
+      A template directory can be copied from #{libexec}/template
 
-    Post-processing scripts are available at #{libexec}/scripts
-  EOS
+      Post-processing scripts are available at #{libexec}/scripts
+    EOS
   end
 
   test do
@@ -53,51 +49,47 @@ end
 
 __END__
 diff --git a/a2fcbmp/a2fcbmp.c b/a2fcbmp/a2fcbmp.c
-index 7fa5d1c..ce0e3ee 100644
+index db8fb35..a8d82f1 100644
 --- a/a2fcbmp/a2fcbmp.c
 +++ b/a2fcbmp/a2fcbmp.c
-@@ -1222,7 +1222,7 @@ unsigned char dlomaincolor[16] = {
- /* -------------------------------------------------------------- */
- /* http://stackoverflow.com/questions/7602919/how-do-i-generate-random-numbers-without-rand-function */
- ushort RandomSeed = (ushort)0xACE1;
--ushort random()
-+ushort a2b_random()
- {
-     ushort bit = ((RandomSeed >> 0) ^ (RandomSeed >> 2) ^ (RandomSeed >> 3) ^ (RandomSeed >> 5) ) & 1;
+@@ -76,6 +76,7 @@
+ /*                    Should now run everywhere (Windows, Linux, OSX)       */
+ /* ------------------------------------------------------------------------ */
  
-@@ -1243,7 +1243,7 @@ int RandomRange(int iMaxValue)
++#include <ctype.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+@@ -115,6 +116,8 @@
+ /* Declarations, Vars. etc.                                                 */
+ /* ------------------------------------------------------------------------ */
  
-   do {
-     /* get random number */
--    iRetVal = (int)random();
-+    iRetVal = (int)a2b_random();
-     /* get a positive value */
-     if (iRetVal < 0) iRetVal *= -1;
++int cmpstr(char *str, char *cmp);
++
+ /* options flags */
  
-diff --git a/src/b2d.h b/src/b2d.h
-index 8167946..82c76a7 100644
---- a/src/b2d.h
-+++ b/src/b2d.h
-@@ -35,6 +35,7 @@ Apple II color or monochrome HGR or DHGR files.
+ int longnames = 1, bmp = 0, bm2 = 0, bmp3 = 0, a2fc = 0, auxbin = 0, tohgr = 0, dhr = 0, frag = 0,
+diff --git a/magick/m2s.c b/magick/m2s.c
+index 9d03dcf..dbd354e 100644
+--- a/magick/m2s.c
++++ b/magick/m2s.c
+@@ -62,6 +62,7 @@ M2S.EXE - WIN32
+ Built under MinGW 5.1.4 (gcc)
+ ----------------------------------------------------------------------- */
+ 
++#include <ctype.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+diff --git a/src/b2d.c b/src/b2d.c
+index ae2980b..6ae3e9d 100644
+--- a/src/b2d.c
++++ b/src/b2d.c
+@@ -277,6 +277,7 @@ documentation can be reviewed for additional information.
  /* ========================== includes ============================= */
  /* ***************************************************************** */
  
-+#include <stdint.h>
- #include "tomthumb.h"
- 
- /* ***************************************************************** */
-@@ -110,10 +111,10 @@ Apple II color or monochrome HGR or DHGR files.
- /* ========================== typedefs ============================= */
- /* ***************************************************************** */
- 
--typedef unsigned char uchar;
--typedef unsigned short ushort;
--typedef unsigned long ulong;
--typedef short sshort;
-+typedef uint8_t uchar;
-+typedef uint16_t ushort;
-+typedef uint32_t ulong;
-+typedef int16_t sshort;
- 
- /* Bitmap Header structures */
- #ifdef MINGW
++#include <ctype.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
