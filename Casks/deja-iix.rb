8ebd2195a0034a3e49241b0dc65dbd2e79f838f3
Lifepillar <lifepillar@lifepillar.me>
2021-04-01 16:07:20 +0200

    Fix Homebrew complaining about deja-iix's version syntax.

--- Casks/deja-iix.rb
+++ Casks/deja-iix.rb
@@ -1,5 +1,5 @@
 cask 'deja-iix' do
-  version '2.0b (109)'
+  version '2.0b-109'
   sha256 'e6f16fade3e1af3ff484345acee25a05403e13b479e52c0818b833c42e3307be'
 
   # unmarked.com was verified as official when first introduced to the cask
@@ -7,7 +7,7 @@ cask 'deja-iix' do
   name 'Deja IIx'
   name 'AppleWorks emulator'
   # Deja IIx has no official home page
-  homepage 'http://apple2history.org/history/ah19/'
+  homepage 'https://apple2history.org/history/ah19/'
 
   app 'Deja IIx.app'
 end
