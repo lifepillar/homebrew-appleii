24e61542e56ed55e616ec95869f4ae339fc8c81f
Lifepillar <lifepillar@lifepillar.me>
2021-04-25 14:44:00 +0200

    Update formula: apple2e (df7a4df30)

--- Formula/apple2e.rb
+++ Formula/apple2e.rb
@@ -2,8 +2,8 @@ class Apple2e < Formula
   desc "Yet another Apple //e emulator"
   homepage "https://github.com/bradgrantham/apple2e"
   url "https://github.com/bradgrantham/apple2e.git",
-    :revision => "b76c734115877d4ccb01c17fa56e757a58e4beb6"
-  version "20201015" # Date of commit
+    :revision => "df7a4df305e2cd0047865a11a93bf41aa751be64"
+  version "20210112" # Date of commit
   head "https://github.com/bradgrantham/apple2e.git"
 
   depends_on "glfw"
