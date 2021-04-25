cd5f6ed0924eac42c2021a936c9cc78d443b01d1
Lifepillar <lifepillar@lifepillar.me>
2021-04-25 14:41:34 +0200

    Update formula: apple2-image-encoder 4.4.1-FINAL

--- Formula/apple2-image-encoder.rb
+++ Formula/apple2-image-encoder.rb
@@ -1,9 +1,9 @@
 class Apple2ImageEncoder < Formula
   desc "Convert a modern image into a format usable by an Apple II"
   homepage "https://github.com/a2geek/apple2-image-encoder"
-  url "https://github.com/a2geek/apple2-image-encoder/archive/4.4.0-FINAL.tar.gz"
-  version "4.4.0"
-  sha256 "493fbeb36121e7d160ab9e3fbb32eaf42dbc4bc4992a692fc1cd60fb7c4caf61"
+  url "https://github.com/a2geek/apple2-image-encoder/archive/4.4.1-FINAL.tar.gz"
+  version "4.4.1"
+  sha256 "e67544a1b6676dac8f76387aaa1d8bf27777b74ff20b59159b7b0300a9be5425"
   head "https://github.com/a2geek/apple2-image-encoder.git"
 
   depends_on "maven" => :build
@@ -11,8 +11,8 @@ class Apple2ImageEncoder < Formula
 
   def install
     system "mvn", "package"
-    libexec.install "target/apple2-image-encoder-4.4.0-FINAL.jar"
-    bin.write_jar_script libexec/"apple2-image-encoder-4.4.0-FINAL.jar", "apple2-image-encoder"
+    libexec.install "target/apple2-image-encoder-4.4.1-FINAL.jar"
+    bin.write_jar_script libexec/"apple2-image-encoder-4.4.1-FINAL.jar", "apple2-image-encoder"
   end
 
   def caveats; <<~EOS
