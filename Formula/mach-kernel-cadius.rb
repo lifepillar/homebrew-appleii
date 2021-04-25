14680960cffd4ecdb16617762d4d2b4eed33a981
Lifepillar <lifepillar@lifepillar.me>
2021-04-25 16:01:42 +0200

    Update formula: mach-kernel-cadius 1.4.5

--- Formula/mach-kernel-cadius.rb
+++ Formula/mach-kernel-cadius.rb
@@ -1,8 +1,8 @@
 class MachKernelCadius < Formula
   desc "BrutalDeluxe's Cadius ProDOS disk imaging utility"
   homepage "https://www.brutaldeluxe.fr/products/crossdevtools/cadius"
-  url "https://github.com/mach-kernel/cadius/archive/1.4.2.tar.gz"
-  sha256 "1360d51fe7f1f9017420d83a3d2a2bee0cd1d60a75033dfb76db13d5d081d5c5"
+  url "https://github.com/mach-kernel/cadius/archive/1.4.5.tar.gz"
+  sha256 "2c88bfa75a3b7ef0df81cfe69665a167228f78299a9885d5b2f0d92cb9ec55af"
 
   def install
     system "make"
