a8043f5bd47d12cd032589ecdec55b95725e05c6
Lifepillar <lifepillar@lifepillar.me>
2021-04-25 16:17:14 +0200

    Fix broken build of Merlin32.

--- Formula/merlin32.rb
+++ Formula/merlin32.rb
@@ -2,19 +2,26 @@ class Merlin32 < Formula
   desc "Multi-pass Cross Assembler for 6502/65c02/65c816 processors"
   homepage "http://www.brutaldeluxe.fr/products/crossdevtools/merlin/"
   url "https://www.brutaldeluxe.fr/products/crossdevtools/merlin/Merlin32_v1.0.zip"
-  sha256 "4dd56aa11ad8f3736e7c7a2295fd5922fd897d2324e006f44c09e0b932b07b4b"
+  sha256 "0de8e9458579221501260a63802dab8cbd96fd524b78f674dc0d7b58d13d929c"
 
   def install
-    cd "Source" do
-      system "make", "-f", "linux_makefile"
-    end
-    bin.install "Source/Merlin32"
+    File.rename "Source", "Merlin32"
+
+    xcodebuild "-configuration", "Release",
+      "-alltargets",
+      "MACOSX_DEPLOYMENT_TARGET=#{MacOS.version}",
+      "SYMROOT=build",
+      "PREFIX=#{prefix}"
+
+    bin.install "build/Release/Merlin32"
     prefix.install "Library" => "lib"
   end
 
-  def caveats; <<~EOS
-    Macro definition files are in #{lib}.
-  EOS
+  def caveats
+    <<~EOS
+      Macro definition files are in #{lib}.
+      The executable is `Merlin32`.
+    EOS
   end
 
   test do
