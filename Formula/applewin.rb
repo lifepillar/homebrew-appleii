f8fa8fe8582c0ea399371928314e59edbbb8d5a3
Lifepillar <lifepillar@lifepillar.me>
2021-04-25 13:39:44 +0200

    Update formula: Applewin 1.30.0.0.
    Remove dependency from wine (removed from Homebrew) and update caveats
    to explain where to get Wine from.

--- Formula/applewin.rb
+++ Formula/applewin.rb
@@ -1,12 +1,10 @@
 class Applewin < Formula
   desc "Apple II emulator for Windows"
   homepage "https://github.com/AppleWin/AppleWin"
-  url "https://github.com/AppleWin/AppleWin/releases/download/v1.29.0.0/AppleWin1.29.0.0.zip"
-  sha256 "b057a46de773df1b410b90df6c486b5a577fd4da5dcc00c8286b9d2508846b68"
+  url "https://github.com/AppleWin/AppleWin/releases/download/v1.30.0.0(try2)/AppleWin1.30.0.0.zip"
+  sha256 "17d2c5e1a29afbaf782b7110a3d5c887251869c3907953dd4ba6c419a214b7c9"
 
   bottle :unneeded
-
-  depends_on "wine"
 
   def install
     libexec.install Dir["*"]
@@ -14,13 +12,18 @@ class Applewin < Formula
     bin.mkpath
     (bin/"applewin").write <<~EOS
       #!/bin/bash
-      wine #{libexec}/Applewin.exe "$@"
+      wine64 #{libexec}/Applewin.exe "$@"
     EOS
   end
 
-  def caveats; <<~EOS
-    The executable is called `applewin`.
-  EOS
+  def caveats
+    <<~EOS
+      Applewin requires Wine. It is recommended to use homebrew-wine:
+
+          https://github.com/Gcenx/homebrew-wine
+
+      The executable is called `applewin`.
+    EOS
   end
 
   test do
