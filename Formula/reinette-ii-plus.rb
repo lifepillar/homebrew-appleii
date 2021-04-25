b32709e751027d92e74782193cfeee8c7d9eefd5
Lifepillar <lifepillar@lifepillar.me>
2021-04-25 16:39:14 +0200

    Update formula: reinette-ii-plus 0.4

--- Formula/reinette-ii-plus.rb
+++ Formula/reinette-ii-plus.rb
@@ -1,8 +1,8 @@
 class ReinetteIiPlus < Formula
-  desc "A French Apple II plus emulator"
+  desc "French Apple II plus emulator"
   homepage "https://github.com/ArthurFerreira2/reinette-II-plus"
-  url "https://github.com/ArthurFerreira2/reinette-II-plus/archive/0.3b.tar.gz"
-  sha256 "f73809580cb026fe5d146b126aa5ee33c0e295d3629e0d4d587d3de0dc6c9c5a"
+  url "https://github.com/ArthurFerreira2/reinette-II-plus/archive/refs/tags/0.4.tar.gz"
+  sha256 "21b7db1336ca0da15bbb3322710c6e8e6942b4421ca19c6ac329a7500572cf2f"
 
   depends_on "sdl2"
 
@@ -24,6 +24,12 @@ class ReinetteIiPlus < Formula
     (bin+"reinetteII+").write shim_script("reinetteII+")
   end
 
+  def caveats
+    <<~EOS
+      The executable is `reinetteII+`.
+    EOS
+  end
+
   test do
     system "false"
   end
@@ -35,15 +41,15 @@ index 32374dd..69ea68f 100644
 --- a/Makefile
 +++ b/Makefile
 @@ -3,10 +3,10 @@ FLAGS = -std=c99 -pedantic -Wpedantic -Wall -O3 -Werror
- 
+
  LIBS = -lSDL2
  # comment this line if you are under Linux :
 -WIN32-LIBS = -lmingw32 -lSDL2main -Wl,-subsystem,windows
 +#WIN32-LIBS = -lmingw32 -lSDL2main -Wl,-subsystem,windows
- 
+
  # comment this line if you are under Linux :
 -WIN32-RES = reinetteII+.res
 +#WIN32-RES = reinetteII+.res
- 
+
  reinetteII+: reinetteII+.c puce6502.c $(WIN32-RES)
         $(CC) $^ $(FLAGS) $(WIN32-LIBS) $(LIBS) -o $@
