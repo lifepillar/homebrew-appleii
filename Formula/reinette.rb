907ef9c3d9df3db609131bed01bbc0fcf994356e
Lifepillar <lifepillar@lifepillar.me>
2021-04-25 16:44:14 +0200

    Audit reinette formula.

--- Formula/reinette.rb
+++ Formula/reinette.rb
@@ -1,8 +1,8 @@
 class Reinette < Formula
-  desc "A French Apple I emulator"
+  desc "French Apple I emulator"
   homepage "https://github.com/ArthurFerreira2/reinette"
   url "https://github.com/ArthurFerreira2/reinette.git",
-    :revision => "e565bee15259ef1661615a6726295bf17a0878c3"
+    revision: "e565bee15259ef1661615a6726295bf17a0878c3"
   version "20191220" # Date of commit
 
   depends_on "ncurses"
