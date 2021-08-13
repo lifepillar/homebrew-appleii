c7f7bee5450ae6265288e90f6848b88004b40a7e
Lifepillar <lifepillar@lifepillar.me>
2021-08-13 14:05:36 +0200

    Update formula: diskbrowser v1.60

--- Formula/diskbrowser.rb
+++ Formula/diskbrowser.rb
@@ -1,8 +1,8 @@
 class Diskbrowser < Formula
   desc "Apple II Disk Browser"
   homepage "https://github.com/dmolony/DiskBrowser"
-  url "https://github.com/dmolony/DiskBrowser/releases/download/1.59/DiskBrowser.jar"
-  sha256 "a119745675214a0790768531f581eff098872eecf9e6e74c800db128126513ce"
+  url "https://github.com/dmolony/DiskBrowser/releases/download/v1.60/DiskBrowser.jar"
+  sha256 "6ace73e027ddbc80a2ba391718688a95805b46ab63251574ca496da275a0c8d2"
 
   depends_on "openjdk"
 
