49265f59c32a3376b8e4dd2fe223ce3824f59d79
Lifepillar <lifepillar@lifepillar.me>
2022-07-13 14:35:13 +0200

    applecommander: Distinguish download URL according to architecture.
    Fixes https://github.com/lifepillar/homebrew-appleii/issues/27

--- Formula/applecommander.rb
+++ Formula/applecommander.rb
@@ -1,13 +1,14 @@
 class Applecommander < Formula
   if Hardware::CPU.intel?
+    url "https://github.com/AppleCommander/AppleCommander/releases/download/1.8.0/AppleCommander-macosx-x86_64-1.8.0.jar"
     sha256 "0b21e2fafa0725b52d4468ce6fc212bd1ee051062d42b2a5079a678e2d2f7220"
   else
+    url "https://github.com/AppleCommander/AppleCommander/releases/download/1.8.0/AppleCommander-macosx-aarch64-1.8.0.jar"
     sha256 "c54e350bc4d9414786a4006a78a49536cbe6b1e2dffa194e44163c068f7d9fc8"
   end
 
   desc "Move data between Apple ][ disk images and native filesystem"
   homepage "https://applecommander.github.io/"
-  url "https://github.com/AppleCommander/AppleCommander/releases/download/1.8.0/AppleCommander-macosx-aarch64-1.8.0.jar"
   depends_on "openjdk"
 
   def install
