1e4629f7cfc5aeefc7600d1b20e60ea75e2a51db
EricFromCanada <enk3@outlook.com>
2023-07-21 13:19:58 -0400

    Update cask: zulufx8 8.0.382,8.72.0.17-ca

--- Casks/zulufx8.rb
+++ Casks/zulufx8.rb
@@ -1,15 +1,16 @@
 cask "zulufx8" do
-  version "8.0.282,8.52.0.23-ca"
+  version "8.0.382,8.72.0.17-ca"
 
-  if Hardware::CPU.intel?
-    sha256 "84393b9430bbb4e5270cc00a0fc1311df6508e85c0bc891bff1ed1d5067da190"
-
-    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-fx-jdk#{version.before_comma}-macosx_x64.dmg",
-      referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
-  else
-    sha256 "e037a306b29d2079719229d43ffaf89908a9c4f7477bf71cd8af718c4285af47"
+  on_arm do
+    sha256 "33b3a54448e4aea4b88741c59ed2025104ae35c7113c3cf380318a4ff34081fd"
 
     url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-fx-jdk#{version.before_comma}-macosx_aarch64.dmg",
+        referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
+  end
+  on_intel do
+    sha256 "e22beea3abeab79cd31164b72125c68e92bdf093661bd04440028630aae50f87"
+
+    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-fx-jdk#{version.before_comma}-macosx_x64.dmg",
         referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   end
 
@@ -19,13 +20,13 @@ cask "zulufx8" do
 
   livecheck do
     url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&ext=dmg&os=macos"
-    regex(/url":"https:.*zulu(.*)-jdk(.*)-macos/i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| match[1] + "," + match[0] }
+    regex(/zulu(.+)-fx-jdk(.+)-macos/i)
+    strategy :json do |json, regex|
+      json["name"].scan(regex).map { |match| match[1] + "," + match[0] }
     end
   end
 
-  pkg "Double-Click to Install ZuluFX #{version.major}.pkg"
+  pkg "Double-Click to Install Azul Zulu JDK #{version.major}.pkg"
 
   uninstall pkgutil: "com.azulsystems.zulu.#{version.major}"
 end
