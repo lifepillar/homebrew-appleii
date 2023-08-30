a310c2365fa77c6db6ecc9e291cfb35cfebf464f
Eric Knibbe <enk3@outlook.com>
2023-08-30 17:50:41 -0400

    Audit: rename "sdl" dependency to "sdl12-compat"

--- Formula/pom1.rb
+++ Formula/pom1.rb
@@ -4,7 +4,7 @@ class Pom1 < Formula
   url "https://downloads.sourceforge.net/project/pom1/pom1/1.0.0/pom1-1.0.0.tar.gz"
   sha256 "8a1088be1628f593c9d00b0e91d47a3820016d4d2b80a4b047cfedf61b34c9c4"
 
-  depends_on "sdl"
+  depends_on "sdl12-compat"
 
   def install
     system "./configure", "--disable-dependency-tracking",
