8aac796cced92e305ebbaa42d34329ea847bf977
Lifepillar <lifepillar@lifepillar.me>
2023-12-02 12:14:39 +0100

    New cask: QuickViewDHGR v0.61

---
+++ Casks/quickviewdhgr.rb
@@ -0,0 +1,11 @@
+cask 'quickviewdhgr' do
+  version '0.61'
+  sha256 :no_check
+
+  url "http://www.apple2works.com/quickviewdhgr.zip"
+  name 'QuickViewDHGR'
+  desc 'Utility for viewing Apple II graphics files'
+  homepage 'http://www.apple2works.com'
+
+  app "QuickViewDHGR.app"
+end
