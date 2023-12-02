001c5caa193e317e20aec40e0de394e9fdf91592
Lifepillar <lifepillar@lifepillar.me>
2023-12-02 12:10:04 +0100

    New cask: NuShrinkItX v1.1

---
+++ Casks/nushrinkitx.rb
@@ -0,0 +1,13 @@
+cask 'nushrinkitx' do
+  version '1.1'
+  sha256 :no_check
+
+  url "http://www.apple2works.com/nushrinkitx.zip"
+  name 'NuShrinkItX'
+  desc 'Utility for creating and working with Apple II archive files (e.g., .shk)'
+  homepage 'http://www.apple2works.com'
+
+  app "NuShrinkItX.app"
+end
+
+
