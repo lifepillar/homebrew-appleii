372e67dca37439d0dc4f379b1216f16334ee14e8
Lifepillar <lifepillar@lifepillar.me>
2023-12-02 12:07:41 +0100

    New cask: CiderXPress v1.0

---
+++ Casks/ciderxpress.rb
@@ -0,0 +1,12 @@
+cask 'ciderxpress' do
+  version '1.0'
+  sha256 :no_check
+
+  url "http://apple2works.com/ciderxpress.zip"
+  name 'CiderXPress'
+  desc 'Utility for creating and working with Apple II disk images'
+  homepage 'http://www.apple2works.com'
+
+  app "CiderXPress.app"
+end
+
