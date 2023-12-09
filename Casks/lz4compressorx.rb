e68709e0a5822ef659dd35b3893aef80c24bd26b
Lifepillar <lifepillar@lifepillar.me>
2023-12-09 10:56:07 +0100

    New cask: LZ4CompressorX v0.8

---
+++ Casks/lz4compressorx.rb
@@ -0,0 +1,11 @@
+cask 'lz4compressorx' do
+  version '0.8'
+  sha256 :no_check
+
+  url "http://www.apple2works.com/marklim/lz4compressorx.zip"
+  name 'LZ4CompressorX'
+  desc 'Utility for general file compression'
+  homepage 'http://www.apple2works.com'
+
+  app "LZ4CompressorX.app"
+end
