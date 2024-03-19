a5909e92e1036e39e23ddb6303bf915103c7e0fc
Lifepillar <lifepillar@lifepillar.me>
2024-03-19 22:50:12 +0100

    Update formula: c2t (commit 6c20c17f)
    The binary compiled from the released v0.997 does not seem to work (it
    runs, but it does not behave correctly). The current master has a new
    `macos` target, which builds binaries that work on macOS (tested only on
    ARM).
    
    See also https://github.com/lifepillar/homebrew-appleii/issues/37.

--- Formula/c2t.rb
+++ Formula/c2t.rb
@@ -1,20 +1,26 @@
 class C2t < Formula
   desc "Retro Code (Apple II, Cosmac VIP) to Tape/Text"
   homepage "https://github.com/datajerk/c2t"
-  url "https://github.com/datajerk/c2t/archive/v0.997.tar.gz"
+  url "https://github.com/datajerk/c2t.git",
+    revision: "6c20c17fd21d522315fa232c49b64adc65cf9460"
+  version "6c20c17f"
   sha256 "bcf3b0fc596b8e77c6647f489e093380419f577e5dd334e902a9158674b43582"
-  head "https://github.com/datajerk/c2t.git"
-
-  depends_on "cc65" => :build
 
   def install
+    ENV.deparallelize
     system "make", "clean"
-    system "make"
-    bin.install "bin/c2t"
-    bin.install "bin/c2t-96h"
+    system "make", "macos"
+
+    if Hardware::CPU.arm?
+      bin.install "bin/c2t_arm" => "c2t"
+      bin.install "bin/c2t-96h_arm" => "c2t-96h"
+    else
+      bin.install "bin/c2t"
+      bin.install "bin/c2t-96h"
+    end
   end
 
   test do
-    system "make", "test" # Fails
+    system "false"
   end
 end
