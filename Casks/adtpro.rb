5e2930d28909f800a826007d655aee150fb1c44e
Eric Knibbe <enk3@outlook.com>
2024-04-26 10:52:27 -0400

    Audit cask: adtpro

--- Casks/adtpro.rb
+++ Casks/adtpro.rb
@@ -3,12 +3,17 @@ cask "adtpro" do
   sha256 "663a4a6c8b0b7d61bcbf5ae0284b014645cb701a00c084c4ab4361df99eae8fc"
 
   adtpro = "ADTPro-#{version}"
-  url "https://github.com/ADTPro/adtpro/releases/download/v_#{version.tr('.', '_')}/#{adtpro}.dmg",
+  url "https://github.com/ADTPro/adtpro/releases/download/v_#{version.tr(".", "_")}/#{adtpro}.dmg",
       verified: "github.com/ADTPro/adtpro/"
   name "ADTPro"
   desc "Apple Disk Transfer ProDOS"
-  homepage "http://adtpro.com/"
+  homepage "https://adtpro.com/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   # Copy the whole folder
-  app "#{adtpro}"
+  app adtpro
 end
