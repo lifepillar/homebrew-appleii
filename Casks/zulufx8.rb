1b69e1a9d860a2d12e3465388de0b5c957346722
Eric Knibbe <enk3@outlook.com>
2024-05-17 17:18:33 -0400

    Update cask: zulufx8 8.0.412,8.78.0.19

--- Casks/zulufx8.rb
+++ Casks/zulufx8.rb
@@ -1,32 +1,30 @@
 cask "zulufx8" do
-  version "8.0.382,8.72.0.17-ca"
+  arch arm: "aarch64", intel: "x64"
 
-  on_arm do
-    sha256 "33b3a54448e4aea4b88741c59ed2025104ae35c7113c3cf380318a4ff34081fd"
+  version "8.0.412,8.78.0.19"
+  sha256 arm:   "bda9050d1720b0b0b5c9850326bf870f505b882a7d1c5a580144f1d9c9c7036f",
+         intel: "0c88a577fee7a0c33ee8e908d2a8686a0d95428b52be4666c9e3bfbf1e1f3361"
 
-    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-fx-jdk#{version.before_comma}-macosx_aarch64.dmg",
-        referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
-  end
-  on_intel do
-    sha256 "e22beea3abeab79cd31164b72125c68e92bdf093661bd04440028630aae50f87"
-
-    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-fx-jdk#{version.before_comma}-macosx_x64.dmg",
-        referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
-  end
-
-  name "ZuluFX 8"
-  desc "Azul Zulu Java 8 Standard Edition Development Kit"
-  homepage "https://www.azul.com/downloads/zulu/zulu-mac/"
+  url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-ca-fx-jdk#{version.csv.first}-macosx_#{arch}.dmg",
+      referer: "https://www.azul.com/downloads/"
+  name "Azul ZuluFX Java 8 Standard Edition Development Kit"
+  desc "OpenJFX distribution from Azul"
+  homepage "https://www.azul.com/downloads/zulu/zulufx/"
 
   livecheck do
     url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&ext=dmg&os=macos"
-    regex(/zulu(.+)-fx-jdk(.+)-macos/i)
+    url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?java_version=#{version.major}&os=macos&arch=#{arch}&ext=dmg&bundle_type=jdk&release_status=ga"
+    regex(/zulu(\d+(?:[._]\d+)*)-ca-fx-jdk(\d+(?:\.\d+)*)-macosx_#{arch}\.dmg/i)
     strategy :json do |json, regex|
-      json["name"].scan(regex).map { |match| match[1] + "," + match[0] }
+      json["url"]&.scan(regex)&.map { |match| "#{match[1]},#{match[0]}" }
     end
   end
+
+  depends_on macos: ">= :mojave"
 
   pkg "Double-Click to Install Azul Zulu JDK #{version.major}.pkg"
 
   uninstall pkgutil: "com.azulsystems.zulu.#{version.major}"
+
+  zap trash: "~/Library/Saved Application State/com.azul.zulu.java.savedState"
 end
