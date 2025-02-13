849a5a766251eaa629fdefe4f420e0ff9042aea9
Eric Knibbe <enk3@outlook.com>
2025-02-13 17:38:19 -0500

    Update formula: LisaEm 1.2.7-2024.07.28

--- Formula/lisaem.rb
+++ Formula/lisaem.rb
@@ -1,42 +1,39 @@
 class Lisaem < Formula
   desc "Fully functional Lisa Emulator™"
   homepage "https://lisaem.sunder.net/"
-  url "https://lisaem.sunder.net/downloads/lisaem-1.2.7-RC3a-2020.08.24-src.tar.xz"
-  version "1.2.7-2020.08.24"
-  sha256 "0920c16c8e0c4896d5070c8d6350aa26d717c6d7dcb46155110767b7e8a5b98c"
-  head "https://github.com/rayarachelian/lisaem.git"
+  url "https://github.com/arcanebyte/lisaem/archive/refs/tags/RC5-2024.07.28.tar.gz"
+  version "1.2.7-2024.07.28"
+  sha256 "3bbb01a0c26399546d8a97cbccbb80db86f8397c1707c22084b8672f8d39a4a3"
+  head "https://github.com/arcanebyte/lisaem.git"
 
   depends_on "sdl2"
   depends_on "xz"
 
   resource "wxWidgets" do
-    url "https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.3/wxWidgets-3.1.3.tar.bz2"
-    sha256 "fffc1d34dac54ff7008df327907984b156c50cff5a2f36ee3da6052744ab554a"
+    url "https://github.com/wxWidgets/wxWidgets/releases/download/v3.2.5/wxWidgets-3.2.5.tar.bz2"
+    sha256 "0ad86a3ad3e2e519b6a705248fc9226e3a09bbf069c6c692a02acf7c2d1c6b51"
   end
 
   def install
     cd "scripts" do
-      resource("wxWidgets").unpack buildpath/"wxWidgets-3.1.3"
-      inreplace "build-wx3.1.2-modern-macosx.sh" do |s|
-        # build for current OS rather than latest SDK
-        s.gsub! "[[ -z \"$MIN_MACOSX_VERSION\" ]] && ", ""
-        s.gsub! "for VER in 3.1.2; do\n", "for VER in 3.1.3; do\n#"
-        # build for only x86_64 on >= 10.14
+      resource("wxWidgets").unpack "wxWidgets-3.2.5"
+      inreplace "build-wx3.2.5-modern-macosx.sh" do |s|
+        # build only 64-bit on >= 10.14
         s.gsub! "\"$OSVER\" > \"macOS-10.14\"", "\"$OSVER\" > \"macOS-10.13\""
-        s.gsub! 'CXXFLAGS="-std=c++0x -stdlib=libc++"', ""
         s.gsub! "--prefix=/usr/local/wx${VER}-${TYPE}", "--prefix=#{buildpath}/../wxWidgets"
         s.gsub! "&& sudo make", "&& make"
       end
-      system "./build-wx3.1.2-modern-macosx.sh"
+      system "./build-wx3.2.5-modern-macosx.sh --no-minimum-macos"
     end
 
+    inreplace "build.sh", "# replace machine type", "SOFTWARE='LisaEm' #"
     inreplace "bashbuild/Darwin.sys", "macosx10", "macosx1"
 
     ENV.prepend_path "PATH", buildpath/"../wxWidgets/bin" # for wx-config
     system "./build.sh", "build", "--quiet", "--no-upx", "--prefix=#{prefix}"
 
-    bin.install Dir["bin/10.*/*"]
     libexec.install "bin/LisaEm.app"
+    bin.install Dir["bin/*.*/*"]
   end
 
   def caveats
