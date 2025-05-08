7299d1edf1f21be6f42b701c0298c0d4d45ebf73
Lifepillar <lifepillar@lifepillar.me>
2025-05-08 20:35:57 +0200

    Update formula: apple1emulator v0.4.0

--- Formula/apple1emulator.rb
+++ Formula/apple1emulator.rb
@@ -1,8 +1,8 @@
 class Apple1emulator < Formula
   desc "Apple I Emulator"
   homepage "https://github.com/alexander-akhmetov/apple1"
-  url "https://github.com/alexander-akhmetov/apple1/archive/refs/tags/0.3.0.tar.gz"
-  sha256 "4639fefee23fb928a449d0011de4a64c15f4fbe1ab0a1ddd5a6ffcd738eb2ff6"
+  url "https://github.com/alexander-akhmetov/apple1/archive/refs/tags/0.4.0.tar.gz"
+  sha256 "62efa62e11971519a893214224b3d6f652d6c9e0857e4406ff827b6a8197bd57"
   head "https://github.com/alexander-akhmetov/apple1.git"
 
   depends_on "rust" => :build
@@ -10,7 +10,7 @@ class Apple1emulator < Formula
   conflicts_with "apple1", because: "both install an `apple1` executable"
 
   def install
-    inreplace "src/main.rs" do |s|
+    inreplace "src/bin.rs" do |s|
       s.sub! "sys/wozmon.bin", "#{libexec}/sys/wozmon.bin"
       s.sub! "sys/replica1.bin", "#{libexec}/sys/replica1.bin"
     end
@@ -24,6 +24,10 @@ class Apple1emulator < Formula
   def caveats
     <<~EOS
       Supporting files have been installed inside #{libexec}/
+      For example:
+
+          apple1 -p #{libexec}/asm/apple1hello.asm
+
     EOS
   end
 
