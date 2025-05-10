a255dc99e113373c715fa24ff356ff020d20f641
Lifepillar <lifepillar@lifepillar.me>
2025-05-10 23:19:53 +0200

    Update formula: PLASMA (aka PLEIADES) v2.20

--- Formula/plasma.rb
+++ Formula/plasma.rb
@@ -1,11 +1,9 @@
 class Plasma < Formula
   desc "Proto Language AsSeMbler for Apple"
   homepage "https://github.com/dschmenk/PLASMA"
-  url "https://github.com/dschmenk/PLASMA/archive/refs/tags/v2.11.tar.gz"
-  sha256 "dc33e568f83d817d8ddaabc8867423981be639905f6b367e9086cff8c12a6b09"
+  url "https://github.com/dschmenk/PLASMA/archive/refs/tags/v2.20.tar.gz"
+  sha256 "cd16a131e50fb02dd28f92ecb6742533f74b437ffaf91aa6e053e3038ad0a958"
   head "https://github.com/dschmenk/PLASMA.git"
-
-  option "with-test", "Verify the build with `make test`"
 
   depends_on "acme"
 
@@ -15,10 +13,9 @@ class Plasma < Formula
 
     cd "src" do
       system "make"
-      system "make", "test" if build.with?("test")
     end
     libexec.install Dir["*"]
-    bin.write_exec_script libexec/"src/plasm"
+    bin.write_exec_script libexec/"src/xplasm"
     bin.write_exec_script libexec/"src/plvm"
   end
 
