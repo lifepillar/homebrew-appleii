675fc8dc4db860a9b4721625e78f1669f1700a82
Rob Greene <robgreene@gmail.com>
2025-07-31 15:09:39 -0500

    Updating AppleCommander to 1.10.1.

--- Formula/applecommander-acx.rb
+++ Formula/applecommander-acx.rb
@@ -1,8 +1,8 @@
 class ApplecommanderAcx < Formula
   desc "Move data between Apple ][ disk images and native filesystem"
   homepage "https://applecommander.github.io/"
-  url "https://github.com/AppleCommander/AppleCommander/releases/download/1.9.0/AppleCommander-acx-1.9.0.jar"
-  sha256 "b884f240f2e4290daaa436597f83e5896afc7182fd123a7437a1190e3f3ef1ce"
+  url "https://github.com/AppleCommander/AppleCommander/releases/download/1.10.1/AppleCommander-acx-1.10.1.jar"
+  sha256 "7782d9b495de76fcb708e6b6b19617cdcce8f357c2bd5f080684898855a1ec79"
 
   depends_on "openjdk"
 
