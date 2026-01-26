16a45bc2be8ff7502ee9db701f534d4bf80866e6
Lifepillar <lifepillar@lifepillar.me>
2026-01-26 09:24:51 +0100

    Update cask: lisaem 2.0.0-2026.01-25

--- Casks/lisaem.rb
+++ Casks/lisaem.rb
@@ -1,22 +1,27 @@
 cask "lisaem" do
-  version "1.2.7-RC4_2022.04.01"
-  sha256 "5b6b8026f57f6ac39986b11d4481f864ab8feb27412fadfae93eb377f4420c02"
+  version "2.0.0,2026.01.25"
+  sha256 "0e0fee7b3d078c807648c9499840fb15d7e888ac505334a519dee5794454aaca"
 
-  url "https://lisaem.sunder.net/downloads/LisaEm_#{version}-macosx.dmg"
+  url "https://github.com/arcanebyte/lisaem/releases/download/#{version.csv.first}/LisaEm-#{version.csv.first}-#{version.csv.second}-MacOS-15.7_arm64.pkg"
   name "LisaEm"
   desc "Apple Lisa emulator"
-  homepage "https://lisaem.sunder.net/"
+  homepage "https://github.com/arcanebyte/lisaem"
 
   livecheck do
-    url "https://lisaem.sunder.net/downloads.html"
-    regex(/href=.*?LisaEm[._-]v?(\d+(?:\.\d+)+[.\w-]*)-macosx.dmg/i)
+    url :url
+    regex(/LisaEm[._-]v?(\d+(?:\.\d+)+[.\w-]*)-MacOS[._-](?:[.0-9])?[._-]arm64\.pkg/i)
   end
 
-  app "LisaEm #{version}.app", target: "LisaEm/LisaEm.app"
+  pkg "LisaEm-#{version.csv.first}-#{version.csv.second}-MacOS-15.7_arm64.pkg"
+
+  uninstall pkgutil: [
+    "net.sunder.lisaem",
+  ]
 
   zap trash: [
     "~/Library/Caches/net.sunder.lisaem",
-    "~/Library/Preferences/LisaEm Preferences",
+    "~/Library/Preferences/lisaem-arm64-15.03-wx3 Preferences.2",
+    "~/Library/Preferences/lisaem-arm64-15.07-wx3 Preferences.2",
     "~/Library/Preferences/lisaem.conf",
     "~/Library/Preferences/net.sunder.lisaem.plist",
     "~/Library/Saved Application State/net.sunder.lisaem.savedState",
